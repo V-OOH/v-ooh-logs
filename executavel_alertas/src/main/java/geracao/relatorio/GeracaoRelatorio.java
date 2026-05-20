e93e748be94129c40c647ca43ad1dd82286d03e7
rminosso <renan.msilva@sptech.school>
2026-05-20 12:39:53 -0300

    adicionando estrutura geracao relatorios

--- executavel_alertas/src/main/java/geracao/relatorio/GeracaoRelatorio.java
+++ executavel_alertas/src/main/java/geracao/relatorio/GeracaoRelatorio.java
@@ -2,5 +2,27 @@ package geracao.relatorio;
 
 // Realizar relatórios
 
+import java.io.FileOutputStream;
+import com.lowagie.text.Document;
+import com.lowagie.text.Paragraph;
+import com.lowagie.text.pdf.PdfWriter;
+
 public class GeracaoRelatorio {
+    public static void main(String[] args) {
+        Document document = new Document();
+        try {
+            PdfWriter.getInstance(document, new FileOutputStream("relatorio.pdf"));
+            document.open();
+            document.add(new Paragraph("Relatorio 100% foda"));
+            System.out.println("PDF gerado com sucesso!");
+        } catch (Exception e) {
+            System.err.println("Erro ao gerar PDF: " + e.getMessage());
+        } finally {
+            if (document.isOpen()) {
+                document.close();
+            }
+        }
+    }
 }
+
+
